2cb0df01665fbd419a757234805a7c39403e8434
yu-san51 <nekonyan.yuki51@gmail.com>
2020-07-23 08:09:47 +0900

    第9回練習

--- test/src/test/Study07.java
+++ test/src/test/Study07.java
@@ -5,6 +5,8 @@ public class Study07 {
 	public static void main(String[] args) {
 		// TODO 自動生成されたメソッド・スタブ
 		Teppan t = new Teppan();
+		t.setTemp();
+		System.out.println("現在の温度は"+t.getTemp()+"度です。");
 		t.setTemp(200);
 		System.out.println("現在の温度は"+t.getTemp()+"度です。");
 		System.out.println(t.yaku());
@@ -13,9 +15,15 @@ public class Study07 {
 }
 class Teppan{//鉄板クラス
 	private int temp=180;//鉄板温度
-	void setTemp(int tmp){
-		temp=tmp;
-		System.out.println("温度を"+temp+"にしました");
+	Teppan(){
+		this.temp=0;
+	}
+	void setTemp() {
+		this.temp=180;
+	}
+	void setTemp(int temp){
+		this.temp=temp;
+		System.out.println("温度を"+this.temp+"にしました");
 	}
 	String yaku(){//焼くメソッド
 		return temp+"度で焼きました";

786f7830b54e831fc39c92278af25341fe5e40ef
yu-san51 <nekonyan.yuki51@gmail.com>
2020-08-01 13:26:25 +0900

    chapter09

---
+++ test/src/test/Chapter09.java
@@ -0,0 +1,16 @@
+package test;
+
+public class Chapter09 {
+
+	public static void main(String[] args) {
+		// TODO 自動生成されたメソッド・スタブ
+		double fuelCost =12.5;
+		double fuelAmount=55.0;
+		
+		Car car = new Car(fuelCost, fuelAmount);
+		
+		car.move(20);
+		System.out.println("残量は、"+ car.getFuelAmount() +"です。");
+	}
+
+}
--- test/src/test/ClassMethod03.java
+++ test/src/test/ClassMethod03.java
@@ -6,12 +6,12 @@ public class ClassMethod03 {
 		// TODO 自動生成されたメソッド・スタブ
 		Human03 yamada = new Human03("山田");
 		Human03 sato = new Human03("佐藤");
-		
+
 		Human03.staticMethodPrint();
 		System.out.println(Human03.humanCount);
-		
+
 		System.out.println(Human03.GREETING);
-		
+
 		yamada.instanceMethodPrint();
 		System.out.println(yamada.name);
 	}

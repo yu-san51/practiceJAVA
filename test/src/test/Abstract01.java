d8e8ef2f64eec9da5ff7674c9201da2e016642c1
yu-san51 <nekonyan.yuki51@gmail.com>
2020-08-03 11:00:23 +0900

    abstract

---
+++ test/src/test/Abstract01.java
@@ -0,0 +1,12 @@
+package test;
+
+public class Abstract01 {
+
+	public static void main(String[] args) {
+		// TODO 自動生成されたメソッド・スタブ
+		Dog dog = new Dog();
+		dog.eat();
+		dog.cry();
+	}
+
+}
---
+++ test/src/test/Animal.java
@@ -0,0 +1,11 @@
+package test;
+
+public abstract class Animal {
+	protected String name;
+
+	public void eat() {
+		System.out.println(name + "が食事をしました");
+	}
+
+	public abstract void cry();
+}

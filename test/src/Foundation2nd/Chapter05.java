98fe2ea46cbd862fb589e2e67322d3135a77f950
yu-san51 <nekonyan.yuki51@gmail.com>
2020-08-06 07:24:18 +0900

    2nd_chapter05

---
+++ test/src/Foundation2nd/Chapter05.java
@@ -0,0 +1,25 @@
+package Foundation2nd;
+
+public class Chapter05 {
+
+	public static void main(String[] args) {
+		// TODO 自動生成されたメソッド・スタブ
+		long long1 = 5555555L;
+		System.out.println(long1);
+		double d1 = 12.345D;
+		System.out.println(d1);
+		boolean bu = true;
+		System.out.println(bu);
+
+		Float fl = 9.8F;
+		System.out.println(fl);
+		Integer int1 = 1;
+		System.out.println(int1);
+		Character ca = 'a';
+		System.out.println(ca);
+		
+		int[] array1 = new int[] {1,2,3,4,5};
+		System.out.println(array1[3]);
+	}
+
+}

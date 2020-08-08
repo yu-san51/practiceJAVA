93749c0b3d4715c72f98f23b5e66694071571ed4
yu-san51 <nekonyan.yuki51@gmail.com>
2020-08-09 08:49:27 +0900

    capsule

---
+++ test/src/practice/capsul/HumanNoCapsule.java
@@ -0,0 +1,11 @@
+package practice.capsul;
+
+public class HumanNoCapsule {
+	public String name;
+	public int age;
+
+	public HumanNoCapsule(String name, int age) {
+		this.name = name;
+		this.age = age;
+	}
+}

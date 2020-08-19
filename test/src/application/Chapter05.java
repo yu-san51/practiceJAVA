0701e2b28a823f22d63ddf9ececa415814f9e2d2
yu-san51 <nekonyan.yuki51@gmail.com>
2020-08-19 16:28:10 +0900

    A_chapter05

--- test/src/application/Chapter05.java
+++ test/src/application/Chapter05.java
@@ -1,5 +1,10 @@
 package application;
 
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.List;
+
 public class Chapter05 {
 
 	public static void main(String[] args) {
@@ -12,6 +17,17 @@ public class Chapter05 {
 			}
 		};
 		System.out.println(a.hello());
+
+		List<String> animals = Arrays.asList("dog", "cat","fox","elephant");
+		System.out.println(animals);
+		Collections.sort(
+				animals,
+				new Comparator<String>() {
+					public int compare(String x, String y) {
+						return x.compareTo(y);
+					}
+				});
+		System.out.println(animals);
 
 	}
 

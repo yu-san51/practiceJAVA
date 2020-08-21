673d5ab090803ae7cba217d1039accbf38cc717d
yu-san51 <nekonyan.yuki51@gmail.com>
2020-08-21 15:04:39 +0900

    ヘッダーパネルの追加

--- JankenGame/src/com/orinigal_game/janken/GameMain.java
+++ JankenGame/src/com/orinigal_game/janken/GameMain.java
@@ -1,13 +1,18 @@
 package com.orinigal_game.janken;
 
-import javax.swing.JFrame;
+import java.awt.BorderLayout;
+import java.awt.Color;
+import java.awt.Dimension;
+
+import javax.swing.JFrame;//ウィンドウの設定
+import javax.swing.JPanel;
 
 public class GameMain {
 
 	public static void main(String[] args) {
 		// TODO 自動生成されたメソッド・スタブ
 
-		//ウインドウの表示
+		//ウインドウの設定
 		//JFrameクラスのインスタンスを作成
 		JFrame frame = new JFrame("じゃんけんゲーム");
 		//ウィンドウのサイズ設定
@@ -19,6 +24,22 @@ public class GameMain {
 		//ウィンドウのサイズを固定→レイアウトの崩れ防止
 		frame.setResizable(false);
 		//見えなかったウィンドウを表示
+		frame.setVisible(true);
+
+		//パネル表示
+		//JPanelクラスのインスタンス生成
+		JPanel headerPanel = new JPanel();
+		//パネルの色を黒に指定
+		headerPanel.setBackground(Color.BLACK);
+		//Dimensionクラスのインスタンスを生成。引数にサイズの設定
+		Dimension headerPanelDimension = new Dimension(640, 50);
+		//上記引数をパネルのサイズにするという設定
+		headerPanel.setPreferredSize(headerPanelDimension);
+		//ここまで設定したパネルをウィンドウ（frame）に追加する
+		//BorderLayout.NORTHでパネルを追加する位置を指定。今回は一番上
+		frame.add(headerPanel, BorderLayout.NORTH);
+
+		//ウィンドウ表示
 		frame.setVisible(true);
 	}
 

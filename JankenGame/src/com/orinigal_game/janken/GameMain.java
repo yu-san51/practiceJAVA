a830f1c0522271bc80e282f1d49a082c45755f3a
yu-san51 <nekonyan.yuki51@gmail.com>
2020-08-22 16:42:21 +0900

    共通部分のメソッド化

--- JankenGame/src/com/orinigal_game/janken/GameMain.java
+++ JankenGame/src/com/orinigal_game/janken/GameMain.java
@@ -23,38 +23,64 @@ public class GameMain {
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		//ウィンドウのサイズを固定→レイアウトの崩れ防止
 		frame.setResizable(false);
-		//見えなかったウィンドウを表示
-		frame.setVisible(true);
 
+		//
 		//パネル表示
 		//JPanelクラスのインスタンス生成
-		JPanel headerPanel = new JPanel();
+		//JPanel headerPanel = new JPanel();
 		//パネルの色を黒に指定
-		headerPanel.setBackground(Color.BLACK);
+		//headerPanel.setBackground(Color.BLACK);
 		//Dimensionクラスのインスタンスを生成。引数にサイズの設定
-		Dimension headerPanelDimension = new Dimension(640, 50);
+		//Dimension headerPanelDimension = new Dimension(640, 50);
 		//上記引数をパネルのサイズにするという設定
-		headerPanel.setPreferredSize(headerPanelDimension);
+		//headerPanel.setPreferredSize(headerPanelDimension);
 		//ここまで設定したパネルをウィンドウ（frame）に追加する
 		//BorderLayout.NORTHでパネルを追加する位置を指定。今回は一番上
+		//frame.add(headerPanel, BorderLayout.NORTH);
+
+		//↓↓↓↓メソッドを使って使いまわせるようにする
+		Dimension headerPanelDimension = new Dimension(640, 50);
+		JPanel headerPanel = setPanel(Color.BLACK, headerPanelDimension);
 		frame.add(headerPanel, BorderLayout.NORTH);
 
+		//
 		//コンテンツパネル
-		JPanel contentsPanel = new JPanel();
-		contentsPanel.setLayout(new BorderLayout());
-		contentsPanel.setBackground(Color.WHITE);
+		//		JPanel contentsPanel = new JPanel();
+		//		contentsPanel.setLayout(new BorderLayout());
+		//		contentsPanel.setBackground(Color.WHITE);
+		//		frame.add(contentsPanel, BorderLayout.CENTER);
+
+		//
+		//↓↓↓↓↓メソッド
+		Dimension contentsPanelDimension = new Dimension(640, 50);
+		JPanel contentsPanel = setPanel(Color.WHITE, contentsPanelDimension);
 		frame.add(contentsPanel, BorderLayout.CENTER);
 
+		//
 		//フッタパネル
-		JPanel footerPanel = new JPanel();
-		footerPanel.setLayout(new BorderLayout());
-		footerPanel.setBackground(Color.BLACK);
+		//		JPanel footerPanel = new JPanel();
+		//		footerPanel.setLayout(new BorderLayout());
+		//		footerPanel.setBackground(Color.BLACK);
+		//		Dimension footerPanelDimension = new Dimension(640, 50);
+		//		footerPanel.setPreferredSize(footerPanelDimension);
+		//		frame.add(footerPanel, BorderLayout.SOUTH);
+
+		//		↓↓↓↓↓メソッド
 		Dimension footerPanelDimension = new Dimension(640, 50);
-		footerPanel.setPreferredSize(footerPanelDimension);
+		JPanel footerPanel = setPanel(Color.BLACK, footerPanelDimension);
 		frame.add(footerPanel, BorderLayout.SOUTH);
 
+		//
 		//ウィンドウ表示
 		frame.setVisible(true);
+	}
+
+	public static JPanel setPanel(Color color, Dimension PanelDimension) {
+		JPanel panel = new JPanel();
+		panel.setPreferredSize(PanelDimension);
+		panel.setLayout(new BorderLayout());
+		panel.setBackground(color);
+		return (panel);
 	}
 
 }

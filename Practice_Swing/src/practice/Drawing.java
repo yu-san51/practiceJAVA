ca5e0695d5484c110fb11647289a5c90f887ccc7
yu-san51 <nekonyan.yuki51@gmail.com>
2020-08-26 08:54:55 +0900

    図形の輪郭描画

--- Practice_Swing/src/practice/Drawing.java
+++ Practice_Swing/src/practice/Drawing.java
@@ -13,7 +13,7 @@ public class Drawing extends JFrame {
 
 	public Drawing() {
 		setDefaultCloseOperation(EXIT_ON_CLOSE);
-		setSize(350, 250);
+		setSize(400, 450);
 		setVisible(true);
 	}
 
@@ -46,6 +46,33 @@ public class Drawing extends JFrame {
 		int[] yPoints2 = { 150, 50, 150 };
 		graphics.drawString("drawPolygon", 180, 30);
 		graphics.drawPolygon(xPoints2, yPoints2, xPoints2.length);
+
+		//閉じた多角形を塗りつぶす fillPolyline(int[] xPoints, int[] yPoints, int nPoints)
+		int[] xPoints3 = { 280, 320, 360 };
+		int[] yPoints3 = { 150, 50, 150 };
+		graphics.drawString("fillPolyline", 280, 30);
+		graphics.fillPolygon(xPoints3, yPoints3, xPoints3.length);
+
+		//図形輪郭の描画
+
+		//一点を指定してそこから四角を作る drawRect(intx, int y, int width, int height)
+		graphics.drawString("drawRecgt", 10, 190);
+		graphics.drawRect(20, 210, 40, 100);
+
+		//drawRectにそれぞれの直径を加えて両端に丸みを帯びる drawRoundRect(int x, int y, iny width, int height, int arcWidth, int arcHeight)
+		graphics.drawString("drawRoundRect", 80, 190);
+		graphics.drawRoundRect(110, 210, 40, 100, 40, 20);
+
+		//楕円形の図形が描写される drawOval(int x, int y, int width, int height)
+		graphics.drawString("drawOval", 180, 190);
+		graphics.drawOval(190, 210, 40, 100);
+
+		//弧を描画 drawArc(int x, int y, int width, int height, int startAngle, int arcAngle)
+		graphics.drawString("drawAct", 250, 190);
+		graphics.drawArc(250, 210, 40, 100, 30, 250);
+
+		//fillRect, fillRoundRect, fillOval, fillArcでそれぞれ塗りつぶすこともできる
+		//drawImage(Image img, int x, int y, ImageOvserver observer)で画像の貼り付けもできる
 	}
 
 }

d9d4e55d281946ead62b9db6a767b2e838876ab3
yu-san51 <nekonyan.yuki51@gmail.com>
2020-08-28 13:19:41 +0900

    ボタンの配置、活性非活性、動作

--- Practice_Swing/src/practice/Button.java
+++ Practice_Swing/src/practice/Button.java
@@ -1,5 +1,10 @@
 package practice;
 
+import java.awt.FlowLayout;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
+
+import javax.swing.JButton;
 import javax.swing.JFrame;
 
 public class Button extends JFrame {
@@ -15,12 +20,32 @@ public class Button extends JFrame {
 
 		init();
 
-		set Visible(true);
+		setVisible(true);
 
 	}
 
 	private void init() {
+		//テキスト付きのボタンJButton(String text)
+		//BorderLayoutだｔｐ最初に配置したボタンが全体に広がってしまうのでFlowを使用
+		setLayout(new FlowLayout());
 
+		JButton button1 = new JButton("test1");
+		add(button1);
+
+		JButton button2 = new JButton("test2");
+		add(button2);
+
+		//設置したボタンの活性非活性
+		button1.setEnabled(true);
+		button2.setEnabled(false);
+
+		//ボタンを押したときの動作の設定
+		button1.addMouseListener(new MouseAdapter() {
+			public void mouseClicked(MouseEvent e) {
+				//オーバーライドで非活性のボタン2をクリックで活性化
+				button2.setEnabled(true);
+			}
+		});
 	}
 
 }

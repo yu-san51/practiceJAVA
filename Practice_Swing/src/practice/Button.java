8ab2addbcd054e8eada1411230a372522d514f5d
yu-san51 <nekonyan.yuki51@gmail.com>
2020-08-28 11:48:09 +0900

    jbutton

---
+++ Practice_Swing/src/practice/Button.java
@@ -0,0 +1,26 @@
+package practice;
+
+import javax.swing.JFrame;
+
+public class Button extends JFrame {
+
+	public static void main(String[] args) {
+		// TODO 自動生成されたメソッド・スタブ
+		new Button();
+	}
+
+	public Button() {
+		setDefaultCloseOperation(EXIT_ON_CLOSE);
+		setSize(350, 250);
+
+		init();
+
+		set Visible(true);
+
+	}
+
+	private void init() {
+
+	}
+
+}

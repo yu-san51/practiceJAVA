b2b02789a309318cfeaff53fccfaa9a83f431beb
yu-san51 <nekonyan.yuki51@gmail.com>
2020-08-29 15:55:32 +0900

    その他ボタンの種類

---
+++ Practice_Swing/src/practice/Button_another.java
@@ -0,0 +1,25 @@
+package practice;
+
+import javax.swing.JFrame;
+
+public class Button_another extends JFrame {
+
+	public static void main(String[] args) {
+		// TODO 自動生成されたメソッド・スタブ
+		new Button_another();
+	}
+
+	public Button_another() {
+		setDefaultCloseOperation(EXIT_ON_CLOSE);
+		setSize(350, 250);
+
+		initJToggleButton();
+
+		setVisible(true);
+	}
+
+	private void initJToggleButton() {
+
+	}
+
+}

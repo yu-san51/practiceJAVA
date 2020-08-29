a67ca7ffa0e939ef3f21a7aa32c1c5e4ad0e0485
yu-san51 <nekonyan.yuki51@gmail.com>
2020-08-29 16:03:38 +0900

    toggleButton

--- Practice_Swing/src/practice/Button_another.java
+++ Practice_Swing/src/practice/Button_another.java
@@ -1,6 +1,12 @@
 package practice;
 
+import java.awt.FlowLayout;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
+
+import javax.swing.JButton;
 import javax.swing.JFrame;
+import javax.swing.JToggleButton;
 
 public class Button_another extends JFrame {
 
@@ -19,7 +25,20 @@ public class Button_another extends JFrame {
 	}
 
 	private void initJToggleButton() {
+		//オンオフのスイッチのような使い方ができる
+		//JToggleButton#isSelectedでボタンが押しているかどうかを取得
+		setLayout(new FlowLayout());
 
+		JToggleButton toggleButton = new JToggleButton("test togglebutton");
+		add(toggleButton);
+
+		JButton button = new JButton("JButton");
+		add(button);
+		button.addMouseListener(new MouseAdapter() {
+			public void mouseClicked(MouseEvent e) {
+				System.out.println(toggleButton.isSelected());
+			}
+		});
 	}
 
 }

3fd3ae9f09d118448f6e4a6b6b4852ca85553397
yu-san51 <nekonyan.yuki51@gmail.com>
2020-09-05 17:35:52 +0900

    JOptionpane

---
+++ Practice_Swing/src/practice/Optionpane.java
@@ -0,0 +1,25 @@
+package practice;
+
+import javax.swing.JFrame;
+
+public class Optionpane extends JFrame{
+
+	public static void main(String[] args) {
+		// TODO 自動生成されたメソッド・スタブ
+		new Optionpane();
+	}
+	
+	public Optionpane() {
+		setDefaultCloseOperation(EXIT_ON_CLOSE);
+		setSize(350, 250);
+		
+		init();
+		
+		setVisible(true);
+	}
+	
+	private void init() {
+		
+	}
+
+}

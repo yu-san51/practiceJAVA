0eab46920a8117ab74d9fd7e5e862d57ba7b9bf9
yu-san51 <nekonyan.yuki51@gmail.com>
2020-09-06 20:40:44 +0900

    JPanel

--- Practice_Swing/src/practice/JPanel_JTabbed.java
+++ Practice_Swing/src/practice/JPanel_JTabbed.java
@@ -1,14 +1,19 @@
 package practice;
 
-import javax.swing.JFrame;
+import java.awt.Color;
+import java.awt.FlowLayout;
 
-public class JPanel_JTabbed extends JFrame{
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.border.LineBorder;
+
+public class JPanel_JTabbed extends JFrame {
 
 	public static void main(String[] args) {
 		// TODO 自動生成されたメソッド・スタブ
 		new JPanel_JTabbed();
 	}
-
 
 	public JPanel_JTabbed() {
 		setDefaultCloseOperation(EXIT_ON_CLOSE);
@@ -20,6 +25,29 @@ public class JPanel_JTabbed extends JFrame{
 	}
 
 	private void init() {
+		setLayout(new FlowLayout());
 
+		//JPanelを3つ設置してJFrameに追加（透明で見えないためsetBorderで境界線）
+		JPanel panel1 = new JPanel();
+		panel1.setBorder(new LineBorder(Color.RED));
+		//addメソッドの使用
+		//JFrameのレイアウトマネージャの初期設定は中央寄せ
+		//2行目の配置を変える場合は
+		//panel1.setLayout(new FLowLayout(FlowLayout.LEFT));
+		panel1.add(new JButton("ボタン1-1"));
+		panel1.add(new JButton("ボタン1-2"));
+		add(panel1);
+
+		JPanel panel2 = new JPanel();
+		panel2.setBorder(new LineBorder(Color.BLUE));
+		panel2.add(new JButton("ボタン2-1"));
+		panel2.add(new JButton("ボタン2-2"));
+		panel2.add(new JButton("ボタン2-3"));
+		add(panel2);
+
+		JPanel panel3 = new JPanel();
+		panel3.setBorder(new LineBorder(Color.YELLOW));
+		panel3.add(new JButton("ボタン3-1"));
+		add(panel3);
 	}
 }

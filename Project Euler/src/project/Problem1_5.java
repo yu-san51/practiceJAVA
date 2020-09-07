778d06a55e5104e39e2623b922b73bbc9bcb8b12
yu-san51 <nekonyan.yuki51@gmail.com>
2020-09-07 17:38:53 +0900

    problem2

--- Project Euler/src/project/Problem1_5.java
+++ Project Euler/src/project/Problem1_5.java
@@ -9,10 +9,14 @@ public class Problem1_5 {
 
 	public Problem1_5() {
 		//problem1 n未満の3または5の倍数の全ての合計を求める
-		System.out.println(sumBelow(10));
+		//		System.out.println(problem1(10));
+
+		//problem2 Fibonacci数列の各行は前の2項を足すことで生成
+		//Fibonacci数列で4百万を超えない項を考え、偶数値の合計を求める
+		problem2();
 	}
 
-	private int sumBelow(int n) {
+	private int problem1(int n) {
 		int sum = 0;
 		for (int i = 1; i < n; i++) {
 			if (i % 3 == 0 || i % 5 == 0) {
@@ -20,6 +24,22 @@ public class Problem1_5 {
 			}
 		}
 		return sum;
+	}
+
+	private void problem2() {
+		int a;
+		int b = 1;
+		int term = 2;
+		int sum = 2;//while文で第3項(termはa+bで作られている)から求めているので初期値を2にしている
+		while (term < 4000000) {
+			a = b;
+			b = term;
+			term = a + b;
+			if (term % 2 == 0) {
+				sum += term;
+			}
+		}
+		System.out.println(sum);
 	}
 
 }

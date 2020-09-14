e3cb223d49c227dbf123f3b613f5487cc9ad5a89
yu-san51 <nekonyan.yuki51@gmail.com>
2020-09-14 22:28:43 +0900

    problem15

--- Project Euler/src/project/Problem14_15.java
+++ Project Euler/src/project/Problem14_15.java
@@ -1,5 +1,7 @@
 package project;
 
+import java.math.BigInteger;
+
 public class Problem14_15 {
 	public static void main(String[] args) {
 		new Problem14_15();
@@ -10,7 +12,10 @@ public class Problem14_15 {
 		//n → 3n + 1 (nが奇数)
 		//この規則にのっとって100万未満の数で開始して最長の数列を得るのは何か？
 		//注意：数列を開始すれば100万を超えても良い。
-		problem14();
+		//problem14();
+
+		//20x20格子で左上の角から初めて右かしただけに進むと何通りのルートがあるか
+		problem15();
 	}
 
 	private void problem14() {
@@ -36,6 +41,23 @@ public class Problem14_15 {
 			return n / 2;
 		} else {
 			return 3 * n + 1;
+		}
+	}
+
+	private final int SUM = 40;
+	private final int CHOICE = 20;
+
+	private void problem15() {
+		BigInteger bigInteger1 = iter15(new BigInteger(String.valueOf(SUM)), SUM - 1, SUM - CHOICE + 1);
+		BigInteger bigInteger2 = iter15(new BigInteger(String.valueOf(CHOICE)), CHOICE - 1, 1);
+		System.out.println(bigInteger1.divide(bigInteger2));
+	}
+
+	private BigInteger iter15(BigInteger num, int mul, int min) {
+		if (mul >= min) {
+			return iter15(num.multiply(new BigInteger(String.valueOf(mul))), mul - 1, min);
+		} else {
+			return num;
 		}
 	}
 }

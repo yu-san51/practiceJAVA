453cfe982c6ec31836651ec9754a095a30145068
yu-san51 <nekonyan.yuki51@gmail.com>
2020-09-17 17:07:19 +0900

    ファイル読み込み、分割のコメントアウト

--- Practice_app/src/app/Hello20200917.java
+++ Practice_app/src/app/Hello20200917.java
@@ -12,7 +12,9 @@ import java.util.Map;
 
 public class Hello20200917 {
 
+	//ファイルのパス
 	private static final String FILE_PATH = "resource\\RAMBLES_IN_FLORIDA_PART_1.txt";
+	//文字の分割 空白が一つ以上、ピリオド、カンマ、セミコロン
 	private static final String SEPARATOR = "(\\s+|\\.|,|;)";
 
 	public static void main(String[] args) {
@@ -22,18 +24,27 @@ public class Hello20200917 {
 		//単語がMapに格納されている上位10位を表示
 
 		//集計
+		//Mapの作成　キーは単語、値は出現数
 		Map<String, Integer> map = new HashMap<>();
+
+		//ファイルの読み込みでエラーが出たときの対処としてtry-catch構文
 		try (FileReader fr = new FileReader(FILE_PATH);
 				BufferedReader br = new BufferedReader(fr)) {
 			String line;
+			//lineに一行ずつ読み込んだモノを格納
 			while ((line = br.readLine()) != null) {
+				//読み込んだ一行分を分割してwords配列に
 				String[] words = line.split(SEPARATOR);
+				//もし
 				for (String word : words) {
 					if (!word.isEmpty()) {
 						if (map.containsKey(word)) {
+							//Mapのcountに出現数を足す
 							int count = map.get(word) + 1;
+							//Mapに格納
 							map.put(word, count);
 						} else {
+							//Mapにキーが含まれていない場合は新たに格納
 							map.put(word, 1);
 						}
 					}
@@ -55,16 +66,19 @@ public class Hello20200917 {
 				maxLengthOfSpelling = key.length();
 			}
 		}
+		//並べ替え Mapから出現数を取得して、その差を返す
 		Collections.sort(list, (o1, o2) -> {
 			return -map.get(o1) + map.get(o2);
 		});
 
 		//上位10件出力
 		System.out.println("出演回数Top10");
+		//出力のフォーマット
 		String format = "%-" + maxLengthOfSpelling + "s: %3d";
 		for (String word : list) {
 			int count = map.get(word);
 			if (10 < count) {
+				//フォーマット通りの出力をさせる
 				System.out.printf(format, word, count);
 				System.out.println();
 			}
